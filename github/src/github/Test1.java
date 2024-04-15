ba0450801c8dd1d574ba04dce9193c0813f4a356
sunilk <sunilk@SPE-ENGINEER-10.protoexpress.com>
2024-04-15 16:44:19 -0700

    Removed println from the code.

--- github/src/github/Test1.java
+++ github/src/github/Test1.java
@@ -4,8 +4,7 @@ public class Test1 {
 
    public static void main(String[] args) {
      System.out.println("Hello Java...");
-     System.out.println("Hello Java2...");
-     System.out.println("Hello Java3...");
+     System.out.println("Hello Java2...");     
    }
 
 }
